98b0ea87b5c39bdebf806af9685e66684ad68a6c
Zycus (VJ) <vijaysanghavi5@gmail.com>
2019-02-01 14:37:45 +0530

    Initial Commit

---
+++ DraggableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DraggableView
+//
+//  Created by Zycus on 01/02/19.
+//  Copyright © 2019 VJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DraggableViewTests/DraggableViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  DraggableViewTests.swift
+//  DraggableViewTests
+//
+//  Created by Zycus on 01/02/19.
+//  Copyright © 2019 VJ. All rights reserved.
+//
+
+import XCTest
+@testable import DraggableView
+
+class DraggableViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
